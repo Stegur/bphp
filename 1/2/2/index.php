792eb8904988e0cee74d207a8afd6995795c68a3
Stegura Alex <stegura.alex@gmail.com>
2019-02-21 23:21:34 +0300

    fix

--- 1/2/2/index.php
+++ 1/2/2/index.php
@@ -5,8 +5,8 @@ $lastName = $_POST['lastName'];
 $middleName = $_POST['middleName'];
 
 $fullName = $lastName . ' ' . $firstName . ' ' . $middleName;
-$fio = $lastName . ' ' . mb_substr($firstName , 0, 1) . '. ' . mb_substr($firstName , 0,1) . '.';
-$surnameAndInitials = mb_substr($lastName,0,1) . mb_substr($firstName , 0, 1) . mb_substr($firstName , 0,1);
+$fio = $lastName . ' ' . mb_substr($firstName, 0, 1) . '. ' . mb_substr($firstName, 0, 1) . '.';
+$surnameAndInitials = mb_substr($lastName, 0, 1) . mb_substr($firstName, 0, 1) . mb_substr($firstName, 0, 1);
 
 echo "Полное имя: $fullName <br>";
 echo "Фамилия и инициалы: $fio <br>";
